bee16fdd5bf926697465be82ee8f7fc2c730f714
Kong Yiji <>
2017-10-17 18:11:50 +0800

    add new function "compress relative path to zip file"

--- compress/compress.py
+++ compress/compress.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 # platform: python3
 
+import os
+from datetime import date
+from zipfile import ZipFile
 from configparser import ConfigParser
 
 class compress_info(object):
@@ -20,16 +23,34 @@ class compress_info(object):
     def get_section(self):
         return self.config.sections()
 
-    # get path option in specify section
-    def get_path(self, section):
-        return self.config.get(section, 'Path')
+    # get source path option in specify section
+    def get_srcpath(self, section):
+        return self.config.get(section, 'SourcePath')
+
+    # get backup path option in specify section
+    def get_despath(self, section):
+        str_today = date.today().strftime('%Y%m%d')
+        return self.config.get(section, 'BackupPath') + os.sep + str_today
+
+def compress(srcpath, zipfilename):
+    with ZipFile(zipfilename, 'w') as zf:
+        for root, dirnames, filenames in os.walk(srcpath):
+            relroot = os.path.relpath(root, start=os.path.dirname(srcpath))
+            # print(relroot)
+            for filename in filenames:
+                # print(os.path.join(root, filename))
+                zf.write(os.path.join(root, filename), arcname=os.path.join(relroot, filename))
 
 if __name__ == '__main__':
     # get paths which need to compress
     ini = compress_info('config.ini')
     ini.read_config()
     sections = ini.get_section()
-    paths = []
     for section in sections:
-        paths.append(ini.get_path(section))
-    print(sections, paths)
+        srcpath = ini.get_srcpath(section)
+        despath = ini.get_despath(section)
+        zipfilename = despath + os.sep + section + '.zip'
+        os.chdir(srcpath)
+        # print(srcpath, despath, zipfilename)
+        compress(srcpath, zipfilename)
+    # print(sections, srcpaths, despaths)
