c0f7741b61941f85f00b4298bd2b38d19344a007
K <>
2018-06-27 17:09:11 +0800

    add log module and some improment

--- compress/compress.py
+++ compress/compress.py
@@ -7,10 +7,12 @@
 import os
 import chardet
 import subprocess
+from logger import log
 from datetime import date
 from zipfile import ZipFile
 from configparser import ConfigParser, NoOptionError
 
+compress_log = log('compress')
 
 class compress_info(object):
     """get information"""
@@ -26,6 +28,7 @@ class compress_info(object):
             cache = f.read()
             filecode = chardet.detect(cache)['encoding']
         self.config.read(self.file, encoding=filecode)
+        compress_log.info('load config file')
 
     # get all sections, except 'DEFAULT'
     def get_section(self):
@@ -40,31 +43,38 @@ class compress_info(object):
         str_today = date.today().strftime('%Y%m%d')
         return self.config.get(section, 'BackupPath') + os.sep + str_today
 
-    # get winrar path
+    # get WinRAR path
     def get_rarpath(self, section):
         return self.config.get(section, 'RARPath')
 
 
 def compress(srcpath, zipfilename):
-    """compress source files to zipfilename"""
+    """compress source files to zipfile name"""
 
     # open file
     with ZipFile(zipfilename, 'w') as zf:
-        # traseval all path in source path
+        # search all path in source path
         for root, dirnames, filenames in os.walk(srcpath):
             # define relative directory
             relroot = os.path.relpath(root, start=os.path.dirname(srcpath))
             # compress all files in relative directory
             for filename in filenames:
                 zf.write(os.path.join(root, filename), arcname=os.path.join(relroot, filename))
+                compress_log.info('compressing file [%s] to [%s]' % (filename, zipfilename))
 
 
 def compress_rar(rarpath, rarfilename, srcpath):
-    """compress source files to rarfilename"""
+    """compress source files to rarfile name"""
 
     # abspath = os.path.join(srcpath, relpath)
-    rar_command = '"%s" a -ep1 -r -m5 -ma5 "%s" "%s"' % (rarpath, rarfilename, srcpath)
+    # command not delete source files
+    rar_command = '"%s" a -ep1 -r -m5 -ma5 -inul "%s" "%s"' % (rarpath, rarfilename, srcpath)
+
+    # command delete source files
+    # rar_command = '"%s" a -ep1 -r -m5 -ma5 -inul -df "%s" "%s"' % (rarpath, rarfilename, srcpath)
+
     subprocess.call(rar_command)
+    compress_log.info('compressing directory [%s] to [%s]' % (srcpath, rarfilename))
 
 
 def main():
@@ -78,12 +88,13 @@ def main():
     # if exists, get WinRAR path
     try:
         rarpath = ini.get_rarpath('DEFAULT') + os.sep + 'Rar.exe'
+        compress_log.info('compress with RAR format')
     except NoOptionError as e:
-        pass
+        compress_log.info('compress with ZIP format')
 
     # compress every section
     for section in sections:
-        # get path infomation from ini file
+        # get path information from ini file
         srcpath = ini.get_srcpath(section)
         despath = ini.get_despath(section)
         zipfilename = despath + os.sep + section + '.zip'
@@ -91,9 +102,11 @@ def main():
 
         # check Source file exists or not
         if not os.path.exists(srcpath):
-            print(section + ' Not Exists')
+            print(section + ' Not Exists, Nothing will be compress.')
+            compress_log.warning('%s not exists. Nothing will be compress.' % section)
+            continue
 
-        # check use Winrar or not to compress
+        # check use WinRAR or not to compress
         if rarpath:
             # check Backup directory exists or not
             # Backup exists, compress
